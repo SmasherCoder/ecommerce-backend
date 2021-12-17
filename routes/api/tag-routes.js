c27bda5bcd125312661f7eb011959743904d2a14
Travis Helms <smasher.coder@gmail.com>
2021-12-16 23:22:34 -0600

    models done

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  const tags = await Tag.findAll({
+    include: Product
+  });
+  return res.status(200).json(tags);
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  const tag = await Tag.findByPk(req.params.id, {
+    include: ProductTag
+  });
+  return res.status(200).json(tag);
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
