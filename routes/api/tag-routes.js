a6d3a21bbfc7879a34b42c3e2e0cb56d6f6a16a8
Travis Helms <smasher.coder@gmail.com>
2021-12-17 23:39:57 -0600

    category routes done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,24 +6,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  const tags = await Tag.findAll({
-    include: Product
-  });
-  return res.status(200).json(tags);
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  const tag = await Tag.findByPk(req.params.id, {
-    include: ProductTag
-  });
-  return res.status(200).json(tag);
 });
 
 router.post('/', (req, res) => {
   // create a new tag
-  
+
 });
 
 router.put('/:id', (req, res) => {
